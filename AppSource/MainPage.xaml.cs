c01fd2fd9d0d05e81180555630ecc9d4cb1bc5ec
Rashid Hasanov <codev01@mail.ru>
2020-10-05 19:25:56 +0300

    //

---
+++ AppSource/MainPage.xaml.cs
@@ -0,0 +1,12 @@
+﻿using Windows.UI.Xaml.Controls;
+
+namespace OxygenVK.AppSource
+{
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+        }
+    }
+}
