d04f7b27f5e88e19ae670e5955aade7940192e60
Rashid Hasanov <codev01@mail.ru>
2020-10-07 20:44:59 +0300

    мелкие правки

--- App.xaml.cs
+++ App.xaml.cs
@@ -1,7 +1,8 @@
-﻿using OxygenVK.AppSource;
-
-using System;
+﻿using System;
 using System.Reflection;
+
+using OxygenVK.AppSource;
+
 using Windows.ApplicationModel;
 using Windows.ApplicationModel.Activation;
 using Windows.ApplicationModel.Core;
@@ -13,65 +14,63 @@ using Windows.UI.Xaml.Navigation;
 
 namespace OxygenVK
 {
-    sealed partial class App : Application
-    {
-        public App()
-        {
-            this.InitializeComponent();
-            this.Suspending += OnSuspending;
-        }
+	public sealed partial class App : Application
+	{
+		public App()
+		{
+			InitializeComponent();
+			Suspending += OnSuspending;
+		}
 
-        protected override void OnLaunched(LaunchActivatedEventArgs e)
-        {
-            Frame rootFrame = Window.Current.Content as Frame;
+		protected override void OnLaunched(LaunchActivatedEventArgs e)
+		{
+			if (!(Window.Current.Content is Frame rootFrame))
+			{
+				rootFrame = new Frame();
 
-            if (rootFrame == null)
-            {
-                rootFrame = new Frame();
+				rootFrame.NavigationFailed += OnNavigationFailed;
 
-                rootFrame.NavigationFailed += OnNavigationFailed;
+				if (e.PreviousExecutionState == ApplicationExecutionState.Terminated)
+				{
+					//TODO: Загрузить состояние из ранее приостановленного приложения
+				}
 
-                if (e.PreviousExecutionState == ApplicationExecutionState.Terminated)
-                {
-                    //TODO: Загрузить состояние из ранее приостановленного приложения
-                }
+				Window.Current.Content = rootFrame;
+			}
 
-                Window.Current.Content = rootFrame;
-            }
+			if (e.PrelaunchActivated == false)
+			{
+				if (rootFrame.Content == null)
+				{
+					rootFrame.Navigate(typeof(MainPage), e.Arguments);
+				}
+				ApplicationViewTitleBar appViewTitleBar = ApplicationView.GetForCurrentView().TitleBar;
+				appViewTitleBar.ButtonBackgroundColor = Colors.Transparent;
+				CoreApplicationViewTitleBar coreTitleBar = CoreApplication.GetCurrentView().TitleBar;
+				coreTitleBar.ExtendViewIntoTitleBar = true;
+				Window.Current.Activate();
+			}
+		}
 
-            if (e.PrelaunchActivated == false)
-            {
-                if (rootFrame.Content == null)
-                {
-                    rootFrame.Navigate(typeof(MainPage), e.Arguments);
-                }
-                ApplicationViewTitleBar appViewTitleBar = ApplicationView.GetForCurrentView().TitleBar;
-                appViewTitleBar.ButtonBackgroundColor = Colors.Transparent;
-                CoreApplicationViewTitleBar coreTitleBar = CoreApplication.GetCurrentView().TitleBar;
-                coreTitleBar.ExtendViewIntoTitleBar = true;
-                Window.Current.Activate();
-            }
-        }
+		private void OnNavigationFailed(object sender, NavigationFailedEventArgs e)
+		{
+			throw new Exception("Failed to load Page " + e.SourcePageType.FullName);
+		}
 
-        void OnNavigationFailed(object sender, NavigationFailedEventArgs e)
-        {
-            throw new Exception("Failed to load Page " + e.SourcePageType.FullName);
-        }
+		private void OnSuspending(object sender, SuspendingEventArgs e)
+		{
+			SuspendingDeferral deferral = e.SuspendingOperation.GetDeferral();
+			//TODO: Сохранить состояние приложения и остановить все фоновые операции
+			deferral.Complete();
+		}
 
-        private void OnSuspending(object sender, SuspendingEventArgs e)
-        {
-            var deferral = e.SuspendingOperation.GetDeferral();
-            //TODO: Сохранить состояние приложения и остановить все фоновые операции
-            deferral.Complete();
-        }
-
-        public static TEnum GetEnum<TEnum>(string text) where TEnum : struct
-        {
-            if (!typeof(TEnum).GetTypeInfo().IsEnum)
-            {
-                throw new InvalidOperationException("Generic parameter 'TEnum' must be an enum.");
-            }
-            return (TEnum)Enum.Parse(typeof(TEnum), text);
-        }
-    }
+		public static TEnum GetEnum<TEnum>(string text) where TEnum : struct
+		{
+			if (!typeof(TEnum).GetTypeInfo().IsEnum)
+			{
+				throw new InvalidOperationException("Generic parameter 'TEnum' must be an enum.");
+			}
+			return (TEnum)Enum.Parse(typeof(TEnum), text);
+		}
+	}
 }
