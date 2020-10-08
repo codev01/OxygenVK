489357af69e973219462170a7e3e3b1b55f9341f
Rashid Hasanov <codev01@mail.ru>
2020-10-08 13:09:42 +0300

    Немного правок Немного комментариев Добавил в настройки возможность включить звуки обратной связи с элементами управления (пространственный звук)

--- AppSource/Views/Settings/SettingsPage.xaml.cs
+++ AppSource/Views/Settings/SettingsPage.xaml.cs
@@ -20,5 +20,38 @@ namespace OxygenVK.AppSource.Views.Settings
 				ThemeHelper.RootTheme = App.GetEnum<ElementTheme>(selectedTheme);
 			}
 		}
+
+		private void soundToggle_Toggled(object sender, RoutedEventArgs e)
+		{
+			if (soundToggle.IsOn == true)
+			{
+				spatialSoundBox.IsEnabled = true;
+				ElementSoundPlayer.State = ElementSoundPlayerState.On;
+			}
+			else
+			{
+				spatialSoundBox.IsEnabled = false;
+				spatialSoundBox.IsChecked = false;
+
+				ElementSoundPlayer.State = ElementSoundPlayerState.Off;
+				ElementSoundPlayer.SpatialAudioMode = ElementSpatialAudioMode.Off;
+			}
+		}
+
+		private void spatialSoundBox_Checked(object sender, RoutedEventArgs e)
+		{
+			if (soundToggle.IsOn == true)
+			{
+				ElementSoundPlayer.SpatialAudioMode = ElementSpatialAudioMode.On;
+			}
+		}
+
+		private void spatialSoundBox_Unchecked(object sender, RoutedEventArgs e)
+		{
+			if (soundToggle.IsOn == true)
+			{
+				ElementSoundPlayer.SpatialAudioMode = ElementSpatialAudioMode.Off;
+			}
+		}
 	}
 }
